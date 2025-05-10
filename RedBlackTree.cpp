aee032666c4c495961a0a5598151d47b74caa712
Ksenia <kmagaramova2006@gmail.com>
2025-05-10 23:43:49 +0400

    добавлено оформление с комментариями

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -1,146 +1,87 @@
-﻿#include <iostream>
+﻿//15. Дано дерево. Вывести все листья.
+#include <iostream>
 using namespace std;
 
 struct Tree {
     int data;
-    char color; // 'r' - красный, 'b' - черный
-    Tree* left, * right, * parent;
+    char color;
+    Tree* left;
+    Tree* right;
+    Tree* parent;
 
+    //по умолчанию цвет красный
     Tree(int val) : data(val), color('r'), left(nullptr), right(nullptr), parent(nullptr) {}
 };
 
 Tree* root = nullptr;
 
-// Вспомогательные функции
+/* ========== ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ========== */
+// Функция возвращает дедушку узла
 Tree* grandparent(Tree* x) {
     if (x && x->parent)
         return x->parent->parent;
     return nullptr;
 }
-
+// Функция возвращает дядю узла (брата родителя)
 Tree* uncle(Tree* x) {
     Tree* g = grandparent(x);
     if (!g) return nullptr;
     return (x->parent == g->left) ? g->right : g->left;
 }
-
+// Функция возвращает брата узла
 Tree* sibling(Tree* x) {
     if (!x->parent) return nullptr;
     return (x == x->parent->left) ? x->parent->right : x->parent->left;
 }
 
-// Повороты
+/* ========== ОСНОВНЫЕ ОПЕРАЦИИ ========== */
+// Левый поворот относительно узла x
 void left_rotate(Tree*& tr, Tree* x) {
     Tree* y = x->right;
     x->right = y->left;
     if (y->left)
         y->left->parent = x;
     y->parent = x->parent;
-
     if (!x->parent)
         tr = y;
     else if (x == x->parent->left)
         x->parent->left = y;
     else
         x->parent->right = y;
-
     y->left = x;
     x->parent = y;
 }
-
+// Правый поворот относительно узла x (симметрично левому)
 void right_rotate(Tree*& tr, Tree* x) {
     Tree* y = x->left;
     x->left = y->right;
     if (y->right)
         y->right->parent = x;
     y->parent = x->parent;
-
     if (!x->parent)
         tr = y;
     else if (x == x->parent->right)
         x->parent->right = y;
     else
         x->parent->left = y;
-
     y->right = x;
     x->parent = y;
 }
 
-// Функции балансировки при вставке
-void insert_case_1(Tree*& tr, Tree* x);
-void insert_case_2(Tree*& tr, Tree* x);
-void insert_case_3(Tree*& tr, Tree* x);
-void insert_case_4(Tree*& tr, Tree* x);
-void insert_case_5(Tree*& tr, Tree* x);
+/* ========== БАЛАНСИРОВКА ПРИ ВСТАВКЕ ========== */
+// Основная функция вставки
+void insert_case1(Tree*& tr, Tree* x);
+void insert_case2(Tree*& tr, Tree* x);
+void insert_case3(Tree*& tr, Tree* x);
+void insert_case4(Tree*& tr, Tree* x);
+void insert_case5(Tree*& tr, Tree* x);
 
-void insert_case_1(Tree*& tr, Tree* x) {
-    if (!x->parent) {
-        x->color = 'b';
-    }
-    else {
-        insert_case_2(tr, x);
-    }
-}
-
-void insert_case_2(Tree*& tr, Tree* x) {
-    if (x->parent->color == 'b') {
-        return;
-    }
-    else {
-        insert_case_3(tr, x);
-    }
-}
-
-void insert_case_3(Tree*& tr, Tree* x) {
-    Tree* u = uncle(x);
-    Tree* g;
-
-    if (u && u->color == 'r') {
-        x->parent->color = 'b';
-        u->color = 'b';
-        g = grandparent(x);
-        g->color = 'r';
-        insert_case_1(tr, g);
-    }
-    else {
-        insert_case_4(tr, x);
-    }
-}
-
-void insert_case_4(Tree*& tr, Tree* x) {
-    Tree* g = grandparent(x);
-
-    if (x == x->parent->right && x->parent == g->left) {
-        left_rotate(tr, x->parent);
-        x = x->left;
-    }
-    else if (x == x->parent->left && x->parent == g->right) {
-        right_rotate(tr, x->parent);
-        x = x->right;
-    }
-    insert_case_5(tr, x);
-}
-
-void insert_case_5(Tree*& tr, Tree* x) {
-    Tree* g = grandparent(x);
-
-    x->parent->color = 'b';
-    g->color = 'r';
-    if (x == x->parent->left && x->parent == g->left) {
-        right_rotate(tr, g);
-    }
-    else {
-        left_rotate(tr, g);
-    }
-}
-
-// 1. Функция вставки нового элемента
 void insert(Tree*& tr, Tree* prev, int x) {
     if (!tr) {
         Tree* n = new Tree(x);
         n->parent = prev;
         tr = n;
-        insert_case_1(root, tr);
+        insert_case1(root, tr);
     }
     else {
         if (x < tr->data) {
@@ -152,119 +93,94 @@ void insert(Tree*& tr, Tree* prev, int x) {
     }
 }
 
-// 2. Функция создания дерева
-void createTree() {
-    int n, val;
-    cout << "Введите количество элементов: ";
-    cin >> n;
-
-    cout << "Введите " << n << " элементов:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> val;
-        insert(root, nullptr, val);
-    }
-}
-
-// 3. Функция симметричного обхода (inorder)
-void inorder(Tree* tr) {
-    if (tr) {
-        inorder(tr->left);
-        cout << tr->data << (tr->color == 'r' ? "(r) " : "(b) ");
-        inorder(tr->right);
-    }
-}
-
-// 4. Функция поиска узла
-Tree* findNode(Tree* tr, int val) {
-    if (!tr) return nullptr;
-    if (val == tr->data)
-        return tr;
-    else if (val < tr->data)
-        return findNode(tr->left, val);
-    else
-        return findNode(tr->right, val);
-}
-
-// 5. Функции для удаления элемента
-void deleteFix(Tree* x);
-void transplant(Tree* u, Tree* v);
-
-void deleteNode(Tree* z) {
-    Tree* y = z;
-    Tree* x;
-    char y_original_color = y->color;
-
-    if (!z->left) {
-        x = z->right;
-        transplant(z, z->right);
-    }
-    else if (!z->right) {
-        x = z->left;
-        transplant(z, z->left);
+// Case 1: Узел является корнем дерева
+void insert_case1(Tree*& tr, Tree* x) {
+    if (!x->parent) {
+        x->color = 'b';
     }
     else {
-        y = z->right;
-        while (y->left) y = y->left;
-        y_original_color = y->color;
-        x = y->right;
-
-        if (y->parent == z) {
-            if (x) x->parent = y;
-        }
-        else {
-            transplant(y, y->right);
-            y->right = z->right;
-            y->right->parent = y;
-        }
-
-        transplant(z, y);
-        y->left = z->left;
-        y->left->parent = y;
-        y->color = z->color;
+        insert_case2(tr, x);
     }
+}
+// Case 2: Родитель черный - дерево остается валидным
+void insert_case2(Tree*& tr, Tree* x) {
+    if (x->parent->color == 'b') {
+        return;
+    }
+    else {
+        insert_case3(tr, x);
+    }
+}
+// Case 3: Родитель и дядя красные
+void insert_case3(Tree*& tr, Tree* x) {
+    Tree* u = uncle(x);
+    Tree* g;
 
-    if (y_original_color == 'b' && x)
-        deleteFix(x);
-
-    delete z;
+    if (u && u->color == 'r') {
+        x->parent->color = 'b';
+        u->color = 'b';
+        g = grandparent(x);
+        g->color = 'r';
+        insert_case1(tr, g);
+    }
+    else {
+        insert_case4(tr, x);
+    }
+}
+// Case 4: Родитель красный, дядя черный (требуются повороты)
+void insert_case4(Tree*& tr, Tree* x) {
+    Tree* g = grandparent(x);
+    if (x == x->parent->right && x->parent == g->left) {
+        left_rotate(tr, x->parent);
+        x = x->left;
+    }
+    else if (x == x->parent->left && x->parent == g->right) {
+        right_rotate(tr, x->parent);
+        x = x->right;
+    }
+    insert_case5(tr, x);
+}
+// Case 5: Завершающий поворот
+void insert_case5(Tree*& tr, Tree* x) {
+    Tree* g = grandparent(x);
+    x->parent->color = 'b';
+    g->color = 'r';
+    if (x == x->parent->left && x->parent == g->left) {
+        right_rotate(tr, g);
+    }
+    else {
+        left_rotate(tr, g);
+    }
 }
 
-void transplant(Tree* u, Tree* v) {
-    if (!u->parent)
-        root = v;
-    else if (u == u->parent->left)
-        u->parent->left = v;
-    else
-        u->parent->right = v;
-
-    if (v)
-        v->parent = u->parent;
-}
-
+/* ========== УДАЛЕНИЕ ЭЛЕМЕНТА ========== */
+// Балансировка после удаления
 void deleteFix(Tree* x) {
     while (x != root && (!x || x->color == 'b')) {
         if (x == x->parent->left) {
             Tree* s = x->parent->right;
+            // Case 1: Брат красный
             if (s && s->color == 'r') {
                 s->color = 'b';
                 x->parent->color = 'r';
                 left_rotate(root, x->parent);
                 s = x->parent->right;
             }
-
+            // Case 2: Оба ребенка брата черные
             if ((!s->left || s->left->color == 'b') &&
                 (!s->right || s->right->color == 'b')) {
                 s->color = 'r';
                 x = x->parent;
             }
             else {
+                // Case 3: Правый ребенок брата черный
                 if (!s->right || s->right->color == 'b') {
                     if (s->left) s->left->color = 'b';
                     s->color = 'r';
                     right_rotate(root, s);
                     s = x->parent->right;
                 }
-
+                // Case 4: Левый ребенок брата черный
                 s->color = x->parent->color;
                 x->parent->color = 'b';
                 if (s->right) s->right->color = 'b';
@@ -273,6 +189,7 @@ void deleteFix(Tree* x) {
             }
         }
         else {
+            // Симметричные случаи для правого поддерева
             Tree* s = x->parent->left;
             if (s && s->color == 'r') {
                 s->color = 'b';
@@ -304,8 +221,72 @@ void deleteFix(Tree* x) {
     }
     if (x) x->color = 'b';
 }
+// Функция для перерасположения поддеревьев
+void transplant(Tree* u, Tree* v) {
+    if (!u->parent) {
+        root = v;
+    }
+    else if (u == u->parent->left) {
+        u->parent->left = v;
+    }
+    else {
+        u->parent->right = v;
+    }
+    if (v) {
+        v->parent = u->parent;
+    }
+}
+// Основная функция удаления
+void deleteNode(Tree* z) {
+    Tree* y = z;
+    Tree* x;
+    char y_original_color = y->color;
 
-// 6. Функция вывода всех листьев
+    if (!z->left) {
+        x = z->right;
+        transplant(z, z->right);
+    }
+    else if (!z->right) {
+        x = z->left;
+        transplant(z, z->left);
+    }
+    else {
+        y = z->right;
+        while (y->left) y = y->left;
+        y_original_color = y->color;
+        x = y->right;
+
+        if (y->parent == z) {
+            if (x) x->parent = y;
+        }
+        else {
+            transplant(y, y->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+        transplant(z, y);
+        y->left = z->left;
+        y->left->parent = y;
+        y->color = z->color;
+    }
+
+    if (y_original_color == 'b' && x)
+        deleteFix(x);
+    delete z;
+}
+
+
+
+// Симметричный обход
+void inorder(Tree* tr) {
+    if (tr) {
+        inorder(tr->left);
+        cout << tr->data << (tr->color == 'r' ? "(r) " : "(b) ");
+        inorder(tr->right);
+    }
+}
+
+// Вывод всех листьев
 void printLeaves(Tree* tr) {
     if (!tr) return;
 
@@ -318,7 +299,7 @@ void printLeaves(Tree* tr) {
     printLeaves(tr->right);
 }
 
-// 7. Функция вывода дерева(изображение будет повернуто так, что корень будет слева, а дети будут уходить вправо)
+// Графический вывод дерева
 void printTree(Tree* tr, int level = 0) {
     if (tr) {
         printTree(tr->right, level + 1);
@@ -326,6 +307,31 @@ void printTree(Tree* tr, int level = 0) {
         cout << tr->data << (tr->color == 'r' ? "(r)" : "(b)") << endl;
         printTree(tr->left, level + 1);
     }
+}
+
+// Создание нового дерева
+void createTree() {
+    root = nullptr;
+    int n, val;
+    cout << "Введите количество элементов: ";
+    cin >> n;
+
+    cout << "Введите " << n << " элементов:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> val;
+        insert(root, nullptr, val);
+    }
+}
+
+// Поиск узла по значению(необходимо для функции удаления элемента)
+Tree* findNode(Tree* tr, int val) {
+    if (!tr) return nullptr;
+    if (val == tr->data)
+        return tr;
+    else if (val < tr->data)
+        return findNode(tr->left, val);
+    else
+        return findNode(tr->right, val);
 }
 
 int main() {
@@ -346,7 +352,6 @@ int main() {
 
         switch (choice) {
         case 1:
-            root = nullptr;
             createTree();
             break;
         case 2:
@@ -359,10 +364,13 @@ int main() {
             cin >> val;
             {
                 Tree* node = findNode(root, val);
-                if (node)
+                if (node) {
                     deleteNode(node);
-                else
+                    cout << "Элемент удален.\n";
+                }
+                else {
                     cout << "Элемент не найден!\n";
+                }
             }
             break;
         case 4:
@@ -376,10 +384,11 @@ int main() {
             cout << endl;
             break;
         case 6:
-            cout << "Дерево:\n";
+            cout << "Структура дерева:\n";
             printTree(root);
             break;
         case 0:
+            cout << "Выход...\n";
             break;
         default:
             cout << "Неверный выбор!\n";
