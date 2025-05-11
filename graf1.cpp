690c40a18e31fbd842a3d37cc5958e9451963c9d
Ksenia <kmagaramova2006@gmail.com>
2025-05-11 20:49:46 +0400

    добавлено решение первой задачи

--- graf1.cpp
+++ graf1.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿//Дан неориентированный граф. Вывести количество вершин, смежных с данной.
+#include <iostream>
 #include <vector>
 #include <set>
 #include <stack>
@@ -39,7 +40,6 @@ void DFS(int n, vector<set<int>> adjList, int start) {
 
 void BFS(int n, vector<set<int>> adjList, int start) {
     vector<bool> visited(n, false);
-    visited.assign(n, false);
     queue<int> q;
     q.push(start);
     visited[start] = true;
@@ -61,52 +61,65 @@ void BFS(int n, vector<set<int>> adjList, int start) {
 int main() {
     setlocale(LC_ALL, "RUS");
     //ввод со списком смежности
-    int n;
-    cout << "Введите количество вершин: ";
-    cin >> n;
+    //int n;
+    //cout << "Введите количество вершин: ";
+    //cin >> n;
+
+    //vector<set<int>> adjList(n);
+
+    //cout << "Введите количество соседей и самих соседей для каждой вершины:\n";
+    //for (int i = 0; i < n; ++i) {
+    //    int k;
+    //    cout << "Вершина " << i << ". Количество соседей: ";
+    //    cin >> k;
+
+    //    if (k > 0) {
+    //        cout << "Соседи вершины " << i << ": ";
+    //        for (int j = 0; j < k; ++j) {
+    //            int neighbor;
+    //            cin >> neighbor;
+    //            if (neighbor < 0 || neighbor >= n) {
+    //                cout << "Ошибка! Неверный номер вершины. Повторите ввод.\n";
+    //                --j; // Повторяем ввод для этого соседа
+    //                continue;
+    //            }
+    //            adjList[i].insert(neighbor);
+    //        }
+    //    }
+    //}
+
+//ввод с ребрами
+    int n, m;
+    cout << "Введите количество вершин и рёбер: ";
+    cin >> n >> m;
 
     vector<set<int>> adjList(n);
 
-    cout << "Введите количество соседей и самих соседей для каждой вершины:\n";
-    for (int i = 0; i < n; ++i) {
-        int k;
-        cout << "Вершина " << i << ". Количество соседей: ";
-        cin >> k;
-
-        if (k > 0) {
-            cout << "Соседи вершины " << i << ": ";
-            for (int j = 0; j < k; ++j) {
-                int neighbor;
-                cin >> neighbor;
-                if (neighbor < 0 || neighbor >= n) {
-                    cout << "Ошибка! Неверный номер вершины. Повторите ввод.\n";
-                    --j; // Повторяем ввод для этого соседа
-                    continue;
-                }
-                adjList[i].insert(neighbor);
-            }
-        }
+    cout << "Введите " << m << " рёбер (пары вершин):\n";
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adjList[u].insert(v);
+        adjList[v].insert(u);
     }
-
-    //ввод с ребрами
-        /*int n, m;
-        cout << "Введите количество вершин и рёбер: ";
-        cin >> n >> m;
-
-        vector<set<int>> adjList(n);
-
-        cout << "Введите " << m << " рёбер (пары вершин):\n";
-        for (int i = 0; i < m; ++i) {
-            int u, v;
-            cin >> u >> v;
-            adjList[u].insert(v);
-            adjList[v].insert(u);
-        }*/
     printAdjList(adjList);
     int start;
     cout << "Стартовая вершина для BFS и DFS: ";
     cin >> start;
     DFS(n, adjList, start);
     BFS(n, adjList, start);
+
+    //решение 1 задачи
+    cout << "\nВведите вершину: ";
+    int num;
+    cin >> num;
+    if (num < 0 || num >= n) {
+        cout << "Ошибка! Неверный номер вершины.";
+    }
+    for (int i = 0; i < n; ++i) {
+        if (i == num) {
+            cout << "Количество вершин, смежных с " << num << " = " << size(adjList[i]);
+        }
+    }
     return 0;
 }
