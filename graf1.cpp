1d9d59d129c0814f91224fc9d0524e5c3f91f7aa
Ksenia <kmagaramova2006@gmail.com>
2025-05-11 19:50:47 +0400

    добавлен ввод списка смежности вместо ввода ребер

--- graf1.cpp
+++ graf1.cpp
@@ -6,6 +6,17 @@
 
 using namespace std;
 
+void printAdjList(const vector<set<int>>& adjList) {
+    cout << "\nТекущий список смежности:\n";
+    for (int i = 0; i < adjList.size(); ++i) {
+        cout << i << ": ";
+        for (int neighbor : adjList[i]) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }
+}
+
 void DFS(int n, vector<set<int>> adjList, int start) {
     vector<bool> visited(n, false);
     stack<int> s;
@@ -49,20 +60,49 @@ void BFS(int n, vector<set<int>> adjList, int start) {
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    //ввод с ребрами
-    int n, m;
-    cout << "Введите количество вершин и рёбер: ";
-    cin >> n >> m;
+    //ввод со списком смежности
+    int n;
+    cout << "Введите количество вершин: ";
+    cin >> n;
 
     vector<set<int>> adjList(n);
 
-    cout << "Введите " << m << " рёбер (пары вершин):\n";
-    for (int i = 0; i < m; ++i) {
-        int u, v;
-        cin >> u >> v;
-        adjList[u].insert(v);
-        adjList[v].insert(u);
+    cout << "Введите количество соседей и самих соседей для каждой вершины:\n";
+    for (int i = 0; i < n; ++i) {
+        int k;
+        cout << "Вершина " << i << ". Количество соседей: ";
+        cin >> k;
+
+        if (k > 0) {
+            cout << "Соседи вершины " << i << ": ";
+            for (int j = 0; j < k; ++j) {
+                int neighbor;
+                cin >> neighbor;
+                if (neighbor < 0 || neighbor >= n) {
+                    cout << "Ошибка! Неверный номер вершины. Повторите ввод.\n";
+                    --j; // Повторяем ввод для этого соседа
+                    continue;
+                }
+                adjList[i].insert(neighbor);
+            }
+        }
     }
+
+    //ввод с ребрами
+        /*int n, m;
+        cout << "Введите количество вершин и рёбер: ";
+        cin >> n >> m;
+
+        vector<set<int>> adjList(n);
+
+        cout << "Введите " << m << " рёбер (пары вершин):\n";
+        for (int i = 0; i < m; ++i) {
+            int u, v;
+            cin >> u >> v;
+            adjList[u].insert(v);
+            adjList[v].insert(u);
+        }*/
+    printAdjList(adjList);
     int start;
     cout << "Стартовая вершина для BFS и DFS: ";
     cin >> start;
