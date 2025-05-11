fc3dcef6186cde42f87ba1cf462ec5d406d6b69e
Ksenia <kmagaramova2006@gmail.com>
2025-05-11 21:05:55 +0400

    добавлено решение второго задания по графам

---
+++ graf2.cpp
@@ -0,0 +1,128 @@
+﻿//Дан неориентированный граф. Вставить в граф ребро, соединяющее вершины A и B.
+#include <iostream>
+#include <vector>
+#include <set>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+void printAdjList(const vector<set<int>>& adjList) {//вывод списка смежности
+    cout << "\nТекущий список смежности:\n";
+    for (int i = 0; i < adjList.size(); ++i) {
+        cout << i << ": ";
+        for (int neighbor : adjList[i]) {
+            cout << neighbor << " ";
+        }
+        cout << endl;
+    }
+}
+
+void DFS(int n, vector<set<int>> adjList, int start) {//обход в глубину
+    vector<bool> visited(n, false);
+    stack<int> s;
+    s.push(start);
+
+    cout << "DFS: ";
+    while (!s.empty()) {
+        int v = s.top();
+        s.pop();
+        if (!visited[v]) {
+            visited[v] = true;
+            cout << v << " ";
+            for (auto it = adjList[v].rbegin(); it != adjList[v].rend(); ++it) {
+                if (!visited[*it]) s.push(*it);
+            }
+        }
+    }
+    cout << endl;
+}
+
+void BFS(int n, vector<set<int>> adjList, int start) {//обход в ширину
+    vector<bool> visited(n, false);
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+
+    cout << "BFS: ";
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+        cout << v << " ";
+        for (int neighbor : adjList[v]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+void addEdge(vector<set<int>>& adjList, int A, int B) {//добавление ребра
+    // Проверка на корректность вершин
+    if (A < 0 || A >= adjList.size() || B < 0 || B >= adjList.size()) {
+        cout << "Ошибка! Неверные номера вершин.\n";
+        return;
+    }
+    // Добавляем ребро в обе стороны (граф неориентированный)
+    adjList[A].insert(B);
+    adjList[B].insert(A);
+}
+int main() {
+    setlocale(LC_ALL, "RUS");
+    //ввод со списком смежности
+    //int n;
+    //cout << "Введите количество вершин: ";
+    //cin >> n;
+
+    //vector<set<int>> adjList(n);
+
+    //cout << "Введите количество соседей и самих соседей для каждой вершины:\n";
+    //for (int i = 0; i < n; ++i) {
+    //    int k;
+    //    cout << "Вершина " << i << ". Количество соседей: ";
+    //    cin >> k;
+
+    //    if (k > 0) {
+    //        cout << "Соседи вершины " << i << ": ";
+    //        for (int j = 0; j < k; ++j) {
+    //            int neighbor;
+    //            cin >> neighbor;
+    //            if (neighbor < 0 || neighbor >= n) {
+    //                cout << "Ошибка! Неверный номер вершины. Повторите ввод.\n";
+    //                --j; // Повторяем ввод для этого соседа
+    //                continue;
+    //            }
+    //            adjList[i].insert(neighbor);
+    //        }
+    //    }
+    //}
+
+//ввод с ребрами
+    int n, m;
+    cout << "Введите количество вершин и рёбер: ";
+    cin >> n >> m;
+
+    vector<set<int>> adjList(n);
+
+    cout << "Введите " << m << " рёбер (пары вершин):\n";
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adjList[u].insert(v);
+        adjList[v].insert(u);
+    }
+    printAdjList(adjList);
+    //задание 2
+    int A, B;
+    cout << "\nДобавить ребро между\nA: ";
+    cin >>A;cout << "B: ";cin >> B;
+    addEdge(adjList, A, B);
+    printAdjList(adjList);
+    int start;
+    cout << "Стартовая вершина для BFS и DFS: ";
+    cin >> start;
+    DFS(n, adjList, start);
+    BFS(n, adjList, start);
+    return 0;
+}
